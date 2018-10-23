784b2b470e68d77f2740e94387da8c7027cd27b0
Atcsy <katona.skmobil@gmail.com>
2018-10-23 22:06:56 +0200

    added like.php with style

---
+++ like.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<style type="text/css">
+	* {
+		font-family: Arial, Helvetica, Sans-serif;
+	}
+	body {
+		background-color: #fff;
+	}
+
+	form {
+		position: absolute;
+		top: 0;
+	}
+
+	</style>
+
+
+
+
+
+
+</body>
+</html>
