a97329e996b27719fa5db31b189a6f7e22c271bc
Atcsy <katona.skmobil@gmail.com>
2018-10-24 11:35:06 +0200

    style profile page

--- profile.php
+++ profile.php
@@ -7,12 +7,20 @@ if(isset($_GET['profile_username'])) {
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
     $user_array = mysqli_fetch_array($user_details_query);
 
-	$num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+    $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+
+
 }
 
 
 
 ?>
+<style type="text/css">
+ .wrapper {
+     margin-left: 0px;
+     padding-left: 0px;
+ }
+</style>
 
 <div class="profile_left">
  		<img src="<?php echo $user_array['profile_pic']; ?>">
