8ea7ea52326e4ed693a493a454bcedaa513f004d
Atcsy <katona.skmobil@gmail.com>
2018-10-30 13:04:50 +0100

    style loaded messages and fix scroll issue

--- messages.php
+++ messages.php
@@ -43,7 +43,7 @@ if($user_to != "new")
     <?php
     if($user_to !="new") {
 		echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>";
-		echo "<div class='loaded_messages'>";
+		echo "<div class='loaded_messages' id='scroll_messages'>";
 			echo $message_obj->getMessages($user_to);
 		echo "</div>";
 	} else {
@@ -65,4 +65,9 @@ if($user_to != "new")
 		?>
 		</form>
 	</div>
+
+	<script>
+		var div= document.getElementById("scroll_messages");
+		div.scrollTop = div.scrollHeight;
+	</script>
 </div>
