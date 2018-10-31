2883acd9219189736787f9845a3889fd75522b67
Atcsy <katona.skmobil@gmail.com>
2018-10-31 20:12:13 +0100

    check the search query and search in the database according to it

--- includes/handlers/ajax_friend_search.php
+++ includes/handlers/ajax_friend_search.php
@@ -7,3 +7,32 @@ $userLoggedIn = $_POST['userLoggedIn'];
 
 $names = explode(" ", $query);
 
+// check the prediction what we are searching for
+if(strpos($query, "_") !== false) { // if we have _ in the query search in the usernames
+	$usersReturned = mysqli_query($con, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
+}
+else if(count($names) == 2) { //if 2 elements in the names array
+	$usersReturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' AND last_name LIKE '%$names[1]%') AND user_closed='no' LIMIT 8");
+}
+else {
+	$usersReturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' OR last_name LIKE '%$names[0]%') AND user_closed='no' LIMIT 8");
+}
+if($query != "") {
+	while($row = mysqli_fetch_array($usersReturned)) {
+
+		$user = new User($con, $userLoggedIn);
+
+		if($row['username'] != $userLoggedIn) {
+			$mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
+		}
+		else {
+			$mutual_friends = "";
+		}
+
+
+
+
+	}
+}
+
+?>
